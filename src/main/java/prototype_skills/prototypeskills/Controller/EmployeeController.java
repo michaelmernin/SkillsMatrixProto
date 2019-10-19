8abb0f1bdfbad7a2c038299be8658051242cadc2
Michael Mernin <michael.mernin@gmail.com>
2019-10-19 14:49:01 -0500

    Update EmployeeController.java

--- src/main/java/prototype_skills/prototypeskills/Controller/EmployeeController.java
+++ src/main/java/prototype_skills/prototypeskills/Controller/EmployeeController.java
@@ -1,7 +1,7 @@
 package prototype_skills.prototypeskills.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.javafx.UnmodifiableArrayList;
+//import com.sun.javafx.UnmodifiableArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
